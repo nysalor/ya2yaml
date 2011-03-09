4a4be62743bcde3d09171378e5e66e542f2471f6
Jun Yokoyama <jun@larus.org>
2011-03-09 13:34:50 +0900

    キーがシンボルの時にそのまま書き出すようにした

--- lib/ya2yaml.rb
+++ lib/ya2yaml.rb
@@ -81,7 +81,7 @@ class Ya2YAML
 				u_sec = (obj.usec != 0) ? sprintf(".%.6d",obj.usec) : ''
 				obj.strftime("%Y-%m-%d %H:%M:%S#{u_sec} #{off_hm}")
 			when 'Symbol'
-				'!ruby/symbol ' + obj.to_s
+				':' + obj.to_s
 			when 'Range'
 				'!ruby/range ' + obj.to_s
 			when 'Regexp'
